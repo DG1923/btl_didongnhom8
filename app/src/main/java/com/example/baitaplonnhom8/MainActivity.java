52b96197076d4f3efb049d99a086cb9f8204cc62
DG1923 <odpaig@outlook.com>
2024-06-13 14:45:33 +0700

    Change activity launcher

--- app/src/main/java/com/example/baitaplonnhom8/MainActivity.java
+++ app/src/main/java/com/example/baitaplonnhom8/MainActivity.java
@@ -17,13 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-    private TextView txtBMI,txtTheTrang;
-    private Button btn_duytri,btn_tangco,btn_giammo;
-    private RecyclerView recyclerView,exerciseRecyclerView;
-    private ExerciseAdapter exerciseAdapter;
-    private vecticalExerciseAdapter vecticalAdapter;
-    private List<Exercise> exerciseList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,34 +28,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        getWidget();
-
-
-        exerciseList = new ArrayList<>();
-        exerciseList.add(new Exercise(R.drawable.a, "Leg exercise", "10 mins", "Bài tập giảm mỡ"));
-        exerciseList.add(new Exercise(R.drawable.c, "Push up", "10 mins", "Bài tập giảm mỡ"));
-        exerciseList.add(new Exercise(R.drawable.b, "Pull up", "10 mins", "Bài tập giảm mỡ"));
-
-        // Thêm các bài tập khác vào danh sách
-
-        exerciseAdapter = new ExerciseAdapter(exerciseList);
-        recyclerView.setAdapter(exerciseAdapter);
-        vecticalAdapter = new vecticalExerciseAdapter(exerciseList);
-        exerciseRecyclerView.setAdapter(vecticalAdapter);
-
     }
-    public void getWidget(){
-        txtBMI = findViewById(R.id.txtBMI);
-        txtTheTrang = findViewById(R.id.txtTheTrang);
-        btn_duytri = findViewById(R.id.btn_duytri);
-        btn_giammo = findViewById(R.id.btn_giammo);
-        btn_tangco = findViewById(R.id.btn_tangco);
-//        listView = findViewById(R.id.fast_warmup);
-        recyclerView = findViewById(R.id.fastWarmup);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        exerciseRecyclerView = findViewById(R.id.exercies);
-        exerciseRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
 
-    }
+
+
 
 }
